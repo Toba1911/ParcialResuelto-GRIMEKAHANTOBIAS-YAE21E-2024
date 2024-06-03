ee47706d179b8cef914e9796caf258729aa619b4
Toba1911 <143050725+Toba1911@users.noreply.github.com>
2024-06-03 20:27:37 -0300

    Subiendo parcial Resuelto...

---
+++ parcial-resuelto/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap' 
+import 'jquery'
+import 'popper.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+const app = createApp(App)
+app.mount('#app')
+
+
